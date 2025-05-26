841fc46f9d035c636f941481e2f60dc8813ba80b
mmusk <mdmuzakkir296@gmail.com>
2025-05-26 22:27:19 +0530

    Feat(): Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{html,js,astro,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
